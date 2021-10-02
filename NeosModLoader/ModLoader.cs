ca8d3232ebc10055cbb4935d19d745330019b65b
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-10-02 10:30:52 -0700

    Update ModLoader.cs

--- NeosModLoader/ModLoader.cs
+++ NeosModLoader/ModLoader.cs
@@ -22,7 +22,7 @@ namespace NeosModLoader
                 return;
             }
 
-            string modDirectory = Directory.GetParent(Process.GetCurrentProcess().MainModule.FileName).FullName + "\\nml_mods";
+            string modDirectory = Directory.GetParent(Process.GetCurrentProcess().MainModule.FileName).FullName + "/nml_mods";
             Logger.DebugInternal($"loading mods from {modDirectory}");
 
             ModAssembly[] modsToLoad = null;
